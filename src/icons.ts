67ded59f1b2f8f5c5e5f7e72f8eca72755d25db0
Jake Cooper <jake.elijah.cooper@gmail.com>
2021-10-04 15:27:45 -0700

    Remove vscode

--- src/icons.ts
+++ src/icons.ts
@@ -149,7 +149,6 @@ export const icons: Icon[] = [
   i("Webpack", { [c]: "webpack.svg" }),
   i("Yarn", { [c]: "yarn.svg" }),
   i("Ackee", { [c]: "ackee.svg" }),
-  i("VSCode", { [c]: "vscode.svg" }, ),
   i("WebAssembly", { [c]: "wasm.svg" }, ["wasm"]),
   i("Windows 10", { [c]: "windows10.svg" }),
   i("Windows Vista", { [c]: "Windows_Vista.svg" }),
