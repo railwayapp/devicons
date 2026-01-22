a57baffc307e8d9c3138a83dc7024c4fa933464c
Jonas Frisell <jonas@frisell.it>
2026-01-22 08:10:45 +0100

    Add FusionAuth icon

--- src/icons.ts
+++ src/icons.ts
@@ -91,6 +91,7 @@ export const icons: Icon[] = [
   i("Flask", { [d]: "flask-dark.svg", [l]: "flask-light.svg" }),
   i("Flutter", { [c]: "flutter.svg" }),
   i("Fresh", { [c]: "fresh.svg" }),
+  i("FusionAuth", { [c]: "fusionauth.svg" }),
   i("F#", { [c]: "fsharp.svg" }, ["fsharp"]),
   i("Gatsby", { [c]: "gatsby.svg" }),
   i("Ghost", { [c]: "ghost.svg" }),
